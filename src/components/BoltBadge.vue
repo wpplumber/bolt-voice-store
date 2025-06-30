cdc535c1633935d840500f30f4a02d403753f96e
Tarik <52907282+wpplumber@users.noreply.github.com>
2025-06-30 10:35:29 +0100

    Update BoltBadge to use uploaded logo

--- src/components/BoltBadge.vue
+++ src/components/BoltBadge.vue
@@ -5,6 +5,6 @@
     target="_blank"
     rel="noopener noreferrer"
   >
-    <img src="https://raw.githubusercontent.com/wpplumber/bolt-voice-storefront/main/public/bolt/black_circle_360x360.png" class="h-16 w-16" allow="autoplay">
+    <img src="/black_circle_360x360.png" class="h-16 w-16" alt="Powered by Bolt.new">
   </a>
 </template>
